5d0a65ae5258a440c6e5e11fb327a80de393e6b9
sleep <sleep@kaituocn.com>
2025-02-27 11:15:01 +0800

    init

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"lalaka-pay/api"
-	"lalaka-pay/model"
 )
 
 //TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
@@ -12,37 +11,4 @@ import (
 func main() {
 	client := api.NewClient("", "", "", "", true)
 	fmt.Println(client)
-	api.Buffer[model.SpecialCreateReq](&model.SpecialCreateReq{
-		OutOrderNo:           "",
-		MerchantNo:           "",
-		VposID:               "",
-		ChannelID:            "",
-		TotalAmount:          0,
-		OrderEfficientTime:   "",
-		NotifyURL:            "",
-		SupportCancel:        0,
-		SupportRefund:        0,
-		SupportRepeatPay:     0,
-		OutUserID:            "",
-		CallbackURL:          "",
-		OrderInfo:            "",
-		TermNo:               "",
-		SplitMark:            "",
-		SettleType:           "",
-		OutSplitInfo:         nil,
-		CounterParam:         "",
-		CounterRemark:        "",
-		BusiTypeParam:        "",
-		SgnInfo:              nil,
-		ProductID:            "",
-		GoodsMark:            "",
-		GoodsField:           "",
-		OrderSceneField:      "",
-		AgeLimit:             "",
-		RepeatPayAutoRefund:  "",
-		RepeatPayNotify:      "",
-		CloseOrderAutoRefund: "",
-		ShopName:             "",
-		InteRouting:          "",
-	})
 }
